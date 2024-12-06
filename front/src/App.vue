70351c0e35eae4107d4af8c4c535bcec02e4bac3
MaxenceBoucquez <maxenceboucquez90@gmail.com>
2024-12-06 04:22:33 +0100

    modale + images qui marchent pas

--- front/src/App.vue
+++ front/src/App.vue
@@ -2,25 +2,37 @@
 import OceanLayer1 from "./components/OceanLayer1.vue";
 import data from "./data/data.json"
 import Depth from "./models/depth.ts";
+import {ref} from "vue";
+import DetailsDialog from "./components/DetailsDialog.vue";
 
 const depths = data as Depth[];
+
+const currentDepth = ref<Depth>();
+const isActive = ref<boolean>(false);
+
+const openModal = (depth: Depth) => {
+  currentDepth.value = depth;
+  console.log(currentDepth.value)
+  isActive.value = true;
+}
+
 </script>
 
 <template>
   <OceanLayer1 />
-  <div v-for="depth in depths" :key="depth.depth" class="d-flex">
-    <div class="d-flex depth">
+  <div v-for="depth in depths" :key="depth.depth" class="d-flex depth">
+    <div>
       <h2>{{ depth.fine?.title }}</h2>
-      <p>{{ depth.fine?.text }}</p>
+      <v-btn color="primary" class="pa-3 mt-3" @click="() => openModal(depth)">Détails</v-btn>
     </div>
   </div>
+  <DetailsDialog class="d-flex w-100 justify-center" v-model="isActive" :depth="currentDepth" :is-open="isActive" @close-dialog="isActive = !isActive" />
 </template>
 
 <style scoped>
 * {
   padding: 0;
   margin: 0;
-  box-sizing: border-box;
 }
 
 .depth:nth-child(odd) {
@@ -31,5 +43,17 @@ const depths = data as Depth[];
 .depth:nth-child(even) {
   justify-content: start;
   padding: 3rem;
+}
+
+@media screen and (max-width: 576px){
+  .depth:nth-child(odd) {
+    justify-content: center;
+    padding: 3rem;
+  }
+
+  .depth:nth-child(even) {
+    justify-content: center;
+    padding: 3rem;
+  }
 }
 </style>
---
+++ front/src/components/DetailsDialog.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-dialog scrollable>
+    <div class="dialog">
+      <div class="mb-3">
+        <span class="text-h5">{{ depth?.fine.title }}</span>
+      </div>
+      <div>
+        <div class="mb-3">
+          <p>{{ depth?.fine.text }}</p>
+        </div>
+      </div>
+      <div>
+        <p>Côté humain : </p>
+        <img :src="depth?.fine.manImgSrc" :alt="depth?.fine.manImgAlt">
+      </div>
+      <div>
+        <p>Côté océanique : </p>
+        <img :src="depth?.fine.OceanImgSrc" :alt="depth?.fine.OceanImgAlt">
+      </div>
+        <v-btn color="primary" @click="closeDialog">Fermer</v-btn>
+    </div>
+  </v-dialog>
+</template>
+
+<script setup lang="ts">
+import Depth from "../models/depth.ts";
+
+const emits = defineEmits(["CloseDialog"])
+// Declare props for the component
+defineProps<{
+  depth: Depth | undefined; // Prop of type Depth
+}>();
+
+// Method to close the dialog
+const closeDialog = () => {
+  emits("CloseDialog")
+};
+</script>
+
+<style scoped>
+.text-h5 {
+  font-weight: bold;
+}
+
+.dialog{
+  position: fixed;
+  flex-direction: column;
+  padding: 2rem 2rem 2rem;
+  margin: 2rem;
+  border: 1px solid #ccc;
+  border-radius: 15px;
+  background-color: #fff;
+  color: black;
+  max-width: 576px;
+  top: 0;
+  right: 0;
+  left: 0;
+}
+</style>
